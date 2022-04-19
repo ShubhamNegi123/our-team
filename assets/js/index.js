e1eaee356134149f83186b3d93ce5d88fe67c6f8
Shubham Negi <95840309+ShubhamNegi123@users.noreply.github.com>
2022-04-19 22:55:00 +0530

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,39 @@
 $(function () {
+  $("#index-about-section").mousemove(function (evt) {
+    let IAWidth = $("#index-about-section").width();
+    let IAHeight = $("#index-about-section").height();
 
-    
-    
-    $(window).scroll(function () {
-        let scroll = $(window).scrollTop();
-        let h =  $(window).height();
-        if(scroll >= h/2){
-            $('.navigation-bar').css({
-                backgroundColor : 'rgba(0,0,0,0.7)',
-            });
-            
-        }else{
-            $('.navigation-bar').css({
-                backgroundColor : 'transparent',
-            })
-        }
+    let mouseX = evt.pageX - $("#index-about-section").offset().left;
+    let mouseY = evt.pageY - $("#index-about-section").offset().top;
+
+    $('.heart-img').css({
+        right : (mouseX * 10 )/100,
+        top : (mouseY * -10 )/100 + 10,
+    });
+  });
+
+  $(".navigation-links a").click(function () {
+    $(".navigation-links a").removeClass("active-nav");
+    $(this).addClass("active-nav");
+  });
+
+  $(window).scroll(function () {
+    let scroll = $(window).scrollTop();
+    let h = $(window).height();
+    if (scroll >= h / 2) {
+      $(".navigation-bar").css({
+        backgroundColor: "rgba(0,0,0,0.7)",
+      });
+      $(".social-media-icons ul a").css({
+        color: "#000",
+      });
+    } else {
+      $(".navigation-bar").css({
+        backgroundColor: "transparent",
+      });
+      $(".social-media-icons ul a").css({
+        color: "#fff",
+      });
+    }
   });
 });
